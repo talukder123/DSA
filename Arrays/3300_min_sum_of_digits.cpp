0f2138ebfa93d2db3d41edfd0ab141e89658f473
Abdus Salam Talukder <salamtalukder.official@gmail.com>
2026-05-29 11:14:32 +0600

    Submitted EID day-02

---
+++ Arrays/3300_min_sum_of_digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        int n = nums.size();
+        int ans = INT_MAX;
+
+        for (int i =0; i<n; i++) {
+            int sum = 0;
+            while (nums[i] != 0) {
+                sum += nums[i] % 10;
+                nums[i] = nums[i] / 10;
+            }
+            ans = min(ans, sum);
+        }
+        return ans;
+    }
+};
