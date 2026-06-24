dcea9c632629aedfc89238d6545c671d3dfd9b9e
Abdus Salam Talukder <salamtalukder.official@gmail.com>
2026-06-24 16:28:15 +0600

    added

---
+++ Arrays/1672_highest_sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& arr) {
+        int n = arr.size();
+
+        int ans = 0;
+
+        for (int i=0; i<n; i++) {
+            int sum = 0;
+            for (int j=0; j<arr[i].size(); j++) {
+                sum += arr[i][j];
+            }
+            ans = max(ans, sum);
+        }
+
+        return ans;
+    }
+};
